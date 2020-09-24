1e7d1570bb9100a84d87c7198d343939add606d4
helenohyeah <ouyang.helen@gmail.com>
2020-09-24 21:47:38 +0000

    Retrieving next 5 passes but in GMT time

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 const { nextISSTimesForMyLocation } = require('./iss.js');
 
 /**
- * { message: 'success',
-  request:
-   { altitude: 100,
-     datetime: 1600979137,
-     latitude: 43.6655,
-     longitude: -79.4378,
-     passes: 5 },
-  response:
-   [ { duration: 575, risetime: 1600981559 },
-     { duration: 655, risetime: 1600987308 },
-     { duration: 620, risetime: 1600993160 },
-     { duration: 617, risetime: 1600999017 },
-     { duration: 654, risetime: 1601004835 } ] }
+{ duration: 655, risetime: 1600987308 }
+{ duration: 620, risetime: 1600993160 }
+{ duration: 617, risetime: 1600999017 }
+{ duration: 654, risetime: 1601004835 }
+{ duration: 593, risetime: 1601010659 }
  */
 nextISSTimesForMyLocation((err, passTimes) => {
-  let results = [];
+  // error case
   if (err) {
     return console.log('Error: ', err);
   }
-
-
-
-  console.log('index', passTimes);
+  // all good, no errors
+  // loop through durations and rise times
+  for (const time of passTimes['response']) {
+    const date = new Date(time['risetime'] * 1000);
+    // console.log(date.toString());
+    // console.log(date.toLocaleDateString());
+    // times.push(`Next pass at ${}`)
+    console.log(`Next pass at ${date.toString()} for ${time['duration']} seconds!`);
+  }
 });
