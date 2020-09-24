fe7a645b1dfab3dcf275010a3d40a78d9f105a9e
helenohyeah <ouyang.helen@gmail.com>
2020-09-24 22:01:24 +0000

    Cleaned up code

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 const { nextISSTimesForMyLocation } = require('./iss.js');
 
-/**
-{ duration: 655, risetime: 1600987308 }
-{ duration: 620, risetime: 1600993160 }
-{ duration: 617, risetime: 1600999017 }
-{ duration: 654, risetime: 1601004835 }
-{ duration: 593, risetime: 1601010659 }
- */
 nextISSTimesForMyLocation((err, passTimes) => {
   // error case
   if (err) {
@@ -15,10 +8,7 @@ nextISSTimesForMyLocation((err, passTimes) => {
   // all good, no errors
   // loop through durations and rise times
   for (const time of passTimes['response']) {
-    const date = new Date(time['risetime'] * 1000);
-    // console.log(date.toString());
-    // console.log(date.toLocaleDateString());
-    // times.push(`Next pass at ${}`)
+    const date = new Date((time['risetime'] * 1000));
     console.log(`Next pass at ${date.toString()} for ${time['duration']} seconds!`);
   }
 });
