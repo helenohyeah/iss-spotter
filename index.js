8ddf0365a90a997d96dab946360c4b82a0f60153
helenohyeah <ouyang.helen@gmail.com>
2020-09-24 20:08:27 +0000

    Completed fetchCoordsByIp function

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const { fetchMyIP, fetchCoordsByIp } = require('./iss.js');
 
-fetchMyIP((err, ip) => {
-  if (err) {
-    console.log('Error: ', err);
-    return;
-  }
-  console.log('IP: ', ip);
-});
+// fetchMyIP((err, ip) => {
+//   // console.log('err: ', err);
+//   // console.log('IP: ', ip);
+// });
 
-fetchCoordsByIp(('70.24.49.72', (err, data) => {
-  console.log('err: ', err);
-  console.log('data: ', data);
-}))
+// fetchCoordsByIp('70.24.49.72', (err, data) => {
+//   console.log('err: ', err);
+//   console.log('data: ', data);
+// });
--- iss.js
+++ iss.js
@@ -13,14 +13,30 @@ const fetchMyIP = (callback) => {
       callback(Error(msg), null);
       return;
     }
-    // all good
+    // all good, data recieved
     const ip = JSON.parse(body);
     callback(err, ip);
   });
 };
 
 const fetchCoordsByIp = (ip, callback) => {
-  // callback(err, data);
+  request(`https://ipvigilante.com/${ip}`, (err, response, body) => {
+    // error if invalid domain, user is offline etc.
+    if (err) {
+      callback(err, null);
+      return;
+    }
+    // error if code is not 200, assume server error
+    if (response.statusCode !== 200) {
+      const msg = (`Status Code: ${response.statusCode} when fetching coordinates. Response: ${body}`);
+      callback(Error(msg), null);
+      return;
+    }
+    // all good, data recieved
+    const data = JSON.parse(body);
+    const coords = { latitude: data['data']['latitude'], longitude: data['data']['longitude'] };
+    callback(err, coords);
+  });
 };
 
 module.exports = { fetchMyIP, fetchCoordsByIp };
